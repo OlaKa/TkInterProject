8dd4b13e4f07507d743ba106dbfb4b2a5312a4fa
Ola Karlsson <ola.karlsson@bahnhof.se>
2017-08-23 13:19:29 +0200

    Initial commit

---
+++ screen.py
@@ -0,0 +1,34 @@
+try:
+    import tkinter
+except ImportError:
+    import Tkinter as tkinter
+
+import os
+mainWindow = tkinter.Tk()
+
+mainWindow.title("Grid Demo")
+mainWindow.geometry("640x480-8-20")
+
+label = tkinter.Label(mainWindow, text="Tkinter Grid Demo")
+label.grid(row=0, column=0,columnspan=3)
+
+mainWindow.columnconfigure(0, weight=1)
+mainWindow.columnconfigure(1, weight=1)
+mainWindow.columnconfigure(2, weight=3)
+mainWindow.columnconfigure(3, weight=3)
+mainWindow.columnconfigure(4, weight=3)
+mainWindow.rowconfigure(0, weight=1)
+mainWindow.rowconfigure(1, weight=10)
+mainWindow.rowconfigure(2, weight=1)
+mainWindow.rowconfigure(3, weight=3)
+mainWindow.rowconfigure(4, weight=3)
+
+fileList = tkinter.Listbox(mainWindow)
+fileList.grid(row=1, column=0,sticky='nsew', rowspan=2)
+fileList.config(border=2, relief='sunken')
+for zone in os.listdir('/Windows/System32'):
+    fileList.insert(tkinter.END, zone)
+
+listScroll = tkinter.Scrollbar()
+mainWindow.mainloop()
+
